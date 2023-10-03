f71e0cdad23cbe170e435720160222addc0eb0c1
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-10-03 16:22:37 -0400

    new changes and new img to the website

--- artistAPI.py
+++ artistAPI.py
@@ -3,20 +3,25 @@ import os
 from pprint import pprint
 
 artistName1 = "bad bunny"
+def main(name):
+    infoReturn = get_Artist(name)
+    return extract_artist_info(infoReturn);
 
 def get_Artist(artistName):
     #search for the artist
     searchArt = f"https://musicbrainz.org/ws/2/artist?query={artistName}&fmt=json"
+    try:
+        responseArtist = requests.get(searchArt).json()
+        return responseArtist
+    except Exception as exc:
+        print(exc)
+        print("The artist was not found, sorry!")
+        return None
 
-    responseArtist = requests.get(searchArt)
-
-    if responseArtist.status_code ==200:
-        infoReturn = extract_artist_info(responseArtist.json())
-        return infoReturn
-    else: 
-        return "No artist found"
 
 def extract_artist_info(jsonData):
-    realName = jsonData['artists'][0]["aliases"][0]['name']
-    #v bgnhmjpprint(realName)
-    return realName
+    if jsonData == None:
+        return None
+    else:
+        realName = jsonData['artists'][0]["aliases"][0]["sort-name"]
+        return realName
